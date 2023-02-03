f3c3ca99bf7a21b6efc1d3d6348a1c20e060f14d
Ilya grigoriev <grigoriev.tver@gmail.com>
2023-02-03 08:49:55 +0300

    add data

--- components/Calculate.vue
+++ components/Calculate.vue
@@ -7,76 +7,80 @@
       </div>
     </div>
     <div class="mt-5 md:col-span-2 md:mt-0">
-      <form action="#" method="POST">
-        <div class="shadow sm:overflow-hidden sm:rounded-md">
-          <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
-            <div class="grid grid-cols-2 gap-6">
-              <div class="col-span-2 sm:col-span-1">
-                <label for="company-website" class="block text-sm font-medium text-gray-700">Год</label>
-                <div class="mt-1 flex rounded-md shadow-sm">
-                  <input id="company-website" type="number" name="company-website" class="block w-full flex-1 rounded-none rounded-l-md rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="2023">
-                </div>
+      <div class="shadow sm:overflow-hidden sm:rounded-md">
+        <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
+          <div class="grid grid-cols-2 gap-6">
+            <div class="col-span-2 sm:col-span-1">
+              <label for="company-website" class="block text-sm font-medium text-gray-700">Зарплата</label>
+              <div class="mt-1 flex rounded-md shadow-sm">
+                <input id="company-website" v-model="salary" type="number" name="company-website" class="block w-full flex-1 rounded-none rounded-l-md rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="50000">
               </div>
-              <div class="col-span-2 sm:col-span-1">
-                <label for="company-website" class="block text-sm font-medium text-gray-700">Зарплата</label>
-                <div class="mt-1 flex rounded-md shadow-sm">
-                  <input id="company-website" type="number" name="company-website" class="block w-full flex-1 rounded-none rounded-l-md rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="50000">
-                </div>
-              </div>
+              <p id="email-description" class="mt-2 text-sm text-gray-500">До уплаты подоходного налога</p>
             </div>
-
-            <div>
-              <label for="about" class="block text-sm font-medium text-gray-700">About</label>
-              <div class="mt-1">
-                <textarea id="about" name="about" rows="3" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="you@example.com"></textarea>
-              </div>
-              <p class="mt-2 text-sm text-gray-500">Brief description for your profile. URLs are hyperlinked.</p>
-            </div>
-
-            <div>
-              <label class="block text-sm font-medium text-gray-700">Photo</label>
-              <div class="mt-1 flex items-center">
-                <span class="inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100">
-                  <svg class="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                  </svg>
-                </span>
-                <button type="button" class="ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Change</button>
-              </div>
-            </div>
-
-            <div>
-              <label class="block text-sm font-medium text-gray-700">Cover photo</label>
-              <div class="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-                <div class="space-y-1 text-center">
-                  <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                  <div class="flex text-sm text-gray-600">
-                    <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500">
-                      <span>Upload a file</span>
-                      <input id="file-upload" name="file-upload" type="file" class="sr-only">
-                    </label>
-                    <p class="pl-1">or drag and drop</p>
-                  </div>
-                  <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-            <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Save</button>
           </div>
         </div>
-      </form>
+        <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+          <button class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" @click="calculate">Рассчитать</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 
 <script>
+import CALENDAR from '@/services/mock/calendaries/2023.json'
+
 export default {
   name: "Calculate",
+  data: () => ({
+    salary: null,
+    year: 2023,
+    payday: 10,
+    advanceDay: 25,
+    data: null,
+  }),
+  computed: {
+    url: (vm) =>
+      `https://raw.githubusercontent.com/xmlcalendar/xmlcalendar.github.io/main/data/ru/${vm.year}/calendar.json`,
+    calendarData: () => CALENDAR,
+  },
+  methods: {
+    calculate() {
+      this.data = this.calendarData.months?.map((item) => {
+        const { days, month } = item;
+        const holidays = days.split(',').map((day) => parseInt(day, 10));
+        const holidaysCount = holidays.length;
+        const workdaysCount =
+          new Date(this.year, month, 0).getDate() - holidaysCount;
+        const payday = this.getPayDay(holidays, this.payday);
+        const advanceDay = this.getPayDay(holidays, this.advanceDay);
+
+        item = {
+          month,
+          year: this.year,
+          holidays: holidaysCount,
+          workdays: {
+            firstHalf: 0,
+            secondHalf: 0,
+          },
+          paydays: [payday, advanceDay],
+        };
+
+        return item;
+      })
+    },
+    getPayDay(holidays, payday) {
+      if (payday < 1) {
+        return null
+      }
+      if (holidays.includes(payday)) {
+        return this.getPayDay(holidays, payday - 1)
+      }
+
+      return payday
+    },
+  },
 }
 </script>
 
