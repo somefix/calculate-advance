fd23599734b5271cb48eeb54d8c25c6d1ed2cf69
Ilya grigoriev <grigoriev.tver@gmail.com>
2024-04-05 22:13:53 +0300

    fix

---
+++ pages/kalkulator-zarplati.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default',
+})
+
+useHead({
+  title: 'Калькулятор зарплаты',
+  meta: [
+    { name: 'description', content: 'Калькулятор зарплаты онлайн' }
+  ],
+})
+</script>
+
+<template>
+  <calculate />
+</template>
