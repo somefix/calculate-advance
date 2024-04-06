2477365ec95b8c9218cd83ffa8cf3eda054fdc59
Ilya grigoriev <grigoriev.tver@gmail.com>
2024-04-06 07:52:43 +0300

    add yandex-verification

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,14 @@ definePageMeta({
   layout: 'default',
 });
 
+useHead({
+  title: 'Калькулятор зарплаты',
+  meta: [
+    { name: 'description', content: 'Калькулятор зарплаты онлайн' },
+    { name: 'yandex-verification', content: 'b15fe8bd630f2147' }
+  ],
+});
+
 const router = useRouter();
 
 router.replace({ path: '/kalkulator-zarplati' });
--- pages/kalkulator-zarplati.vue
+++ pages/kalkulator-zarplati.vue
@@ -9,7 +9,7 @@ useHead({
     { name: 'description', content: 'Калькулятор зарплаты онлайн' },
     { name: 'yandex-verification', content: 'b15fe8bd630f2147' }
   ],
-})
+});
 </script>
 
 <template>
